607cab1aa02c9bd30f66b391add6d061a132c716
hjames2012 <h.c.james2012@outlook.com>
2025-05-30 15:24:57 +0000

    ewfws

--- ai-image-recognition-app/src/recognition/model.py
+++ ai-image-recognition-app/src/recognition/model.py
@@ -1,35 +1,11 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import load_img, img_to_array
-import numpy as np
-from PIL import Image
-import io
-import cv2
 import face_recognition
+import numpy as np
+import io
 import os
 from skimage import transform as trans
+import cv2
 
 DB_DIR = os.path.join(os.path.dirname(__file__), "../../face_db")
-
-class ImageRecognitionModel:
-    def __init__(self, model_path):
-        self.model = load_model(model_path)
-
-    def preprocess_image(self, img_path):
-        img = load_img(img_path, target_size=(224, 224))  # Adjust size as needed
-        img_array = img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0  # Normalize the image
-        return img_array
-
-    def predict(self, img_path):
-        processed_image = self.preprocess_image(img_path)
-        predictions = self.model.predict(processed_image)
-        return predictions.argmax(axis=1)  # Return the index of the highest probability class
-
-    def load_labels(self, labels_path):
-        with open(labels_path, 'r') as f:
-            labels = f.read().splitlines()
-        return labels
 
 def load_model():
     known_encodings = []
